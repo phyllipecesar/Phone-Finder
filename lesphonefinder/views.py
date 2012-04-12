2e72a48faa73e9d9bb6b6ffcd7b0a6d4a1fcc19f
Phyllipe Medeiros <phyllipecesar@gmail.com>
2012-04-11 22:47:20 -0300

    Login system has been changed

--- lesphonefinder/views.py
+++ lesphonefinder/views.py
@@ -10,17 +10,39 @@ lang_message = {
     'PORTUGUESE': {
         'INFO_PHONE_FINDER':u"""Phone Finder é um aplicativo que permite o usuário saber a localização do seu celular via web a partir do GPS do seu aparelho.<br />
 Além disso o aplicativo possibilita acesso remoto ao seu celular, isto é, quando você não estiver encontrando seu celular e o mesmo estiver no silencioso, você não precisa perder seu tempo procurando pela sua residência. Você pode ativar pelo nosso website que o seu telefone toque algum tipo de alarme, facilitando a busca do aparelho!<br />
-Além do mais, se o seu telefone foi roubado você pode ativar o modo de tirar fotos automaticamente, isto é, a cada 10 minutos o aparelho irá tirar uma foto e enviar para nosso website, assim você pode tentar reconhecer o local, ou até mesmo os suspeitos."""
+Além do mais, se o seu telefone foi roubado você pode ativar o modo de tirar fotos automaticamente, isto é, a cada 10 minutos o aparelho irá tirar uma foto e enviar para nosso website, assim você pode tentar reconhecer o local, ou até mesmo os suspeitos.""",
+        'LOGIN_ERROR_BLANK':u"Usuário e senha não podem ser vazios.",
+        'LOGIN_ERROR':u"Usuário ou senha inválido.",
+        
         },
+    
     'ENGLISH': {
         'INFO_PHONE_FINDER':u"""
-"""
+""",
+        'LOGIN_ERROR_BLANK':"Username and password can't be blank.",
+        'LOGIN_ERROR':"Username or password mismatches.",
         }
 }
 
     
 def home(request):
+    user = request.user
 
+    if not user.is_active:
+        try:
+            form_username = request.POST['username']
+            form_password = request.POST['password']
+            user = authenticate(username=form_username, password=form_password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+            else:
+                login_error = lang_message['PORTUGUESE']['LOGIN_ERROR']
+        except:
+            if request.POST.has_key('username') or request.POST.has_key('password'):
+                login_error = lang_message['PORTUGUESE']['LOGIN_ERROR_BLANK']
+            
+                
     try:
         ob = Location.objects.all()[0]
         CUR_LATI = ob.lati
